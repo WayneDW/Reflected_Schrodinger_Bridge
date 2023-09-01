e1d91b24eff83a5200201566458d1cfbbaefe810
Wei Deng <greyman@live.cn>
2023-09-01 15:16:09 -0400

    initial Github version - FB-SDE code script without joint training module

---
+++ SB-FBSDE/configs/default_gmm_config.py
@@ -0,0 +1,43 @@
+import ml_collections
+
+def get_gmm_default_configs():
+  config = ml_collections.ConfigDict()
+  # training
+  config.training = training = ml_collections.ConfigDict()
+  config.seed=42 #The utlimate answer of UNIVERSE!
+  config.T = 1.0
+  config.interval = 100
+  config.train_method = 'alternate'
+  config.t0 = 1e-3 ####### simple sde is 0, now set to 1e-3 following MNIST config file # this part is quite quite important to avoid nan loss in DSM-warm_up
+  config.problem_name = 'gmm'
+  config.num_itr = 1000
+  config.eval_itr = 200
+  config.forward_net = 'toy'
+  config.backward_net = 'toy'
+  config.use_arange_t = True
+  config.num_epoch = 1
+  config.num_stage = 8
+  config.train_bs_x = 1024
+  config.sde_type = 'vp'
+  # sampling
+  config.samp_bs = 256
+  config.sigma_min = 0.1
+  config.sigma_max = 2
+  config.snapshot_freq = 1
+  # optimization
+  # config.optim = optim = ml_collections.ConfigDict()
+  config.weight_decay = 0
+  config.optimizer = 'AdamW'
+  config.lr = 1e-3
+  config.lr_gamma = 0.9
+
+  model_configs=None
+
+
+  """ alternative hyperparameters """
+  config.train_bs_x_dsm = 192
+  config.train_bs_t_dsm = 192
+  #config.train_bs_t = 4
+  config.num_itr_dsm = 10000 # # 5000 #100 # 5000
+  config.DSM_warmup = True
+  return config, model_configs
---
+++ SB-FBSDE/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+# conda activate sb-fbsde
+
+
+python main.py --problem-name gmm --forward-net toy  --backward-net toy --dir output --log-tb
+
+#python main.py --problem-name spiral --forward-net toy  --backward-net toy --dir output --log-tb
+
+
+#python main.py --problem-name moon-to-spiral --forward-net toy  --backward-net toy --dir output --log-tb
+
+#python main.py --problem-name gmm --forward-net linear  --backward-net toy --dir output --log-tb
+
+#python main.py --problem-name checkerboard --forward-net toy  --backward-net toy --dir output --log-tb
+
