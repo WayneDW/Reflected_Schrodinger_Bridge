383660577b4b9bdc26bc3b32995afad3449d7e45
Wei Deng <greyman@live.cn>
2023-09-04 09:54:10 -0400

    .

--- SB-FBSDE/grid_search.py
+++ SB-FBSDE/grid_search.py
@@ -34,7 +34,8 @@ Moon-to-Spiral
 
 
 for i in range(20):
-    name = np.random.choice(['moon-to-spiral', 'smile-to-checkerboard'])
+    #name = np.random.choice(['moon-to-spiral', 'smile-to-checkerboard'])
+    name = 'smile-to-checkerboard'
     lr = np.random.choice([1e-3, 6e-4, 3e-4])
     lr_gamma = np.random.choice([0.8])
     t0 = np.random.choice([0])
@@ -51,7 +52,7 @@ for i in range(20):
         output_folder += f'_sigma_min_{sigma_min}_max_{sigma_max}'
     elif sde_type == 'vp':
         beta_min = np.random.choice([0.03, 0.05, 0.1])
-        beta_max = np.random.choice([1, 3])
+        beta_max = np.random.choice([0.6, 1, 3])
         running_comment += f' --beta-min {beta_min} --beta-max {beta_max}'
         output_folder += f'_beta_min_{beta_min}_max_{beta_max}'
     running_comment += f' --lr {lr} --lr-gamma {lr_gamma} --t0 {t0} --sde-type {sde_type} --num-itr {num_itr} --num-stage {stage} --dir {output_folder}'
