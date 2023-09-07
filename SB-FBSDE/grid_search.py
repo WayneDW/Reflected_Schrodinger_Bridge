a265efe03fcb6e98e0366d9823dd303519ee3da1
Wei Deng <greyman@live.cn>
2023-09-07 14:16:49 -0400

    .

--- SB-FBSDE/grid_search.py
+++ SB-FBSDE/grid_search.py
@@ -36,7 +36,7 @@ Moon-to-Spiral
 for i in range(10):
     #name = np.random.choice(['moon-to-spiral', 'smile-to-checkerboard'])
     name = 'smile-to-checkerboard'
-    lr = np.random.choice([1e-3, 6e-4, 3e-4])
+    lr = np.random.choice([1e-3, 6e-4, 3e-4, 1e-4])
     lr_gamma = np.random.choice([0.8])
     t0 = np.random.choice([0])
     sde_type = np.random.choice(['vp', 've', 'simple'])
@@ -46,13 +46,13 @@ for i in range(10):
     output_folder = f'{name}_lr_{lr}_gamma_{lr_gamma}_t0_{t0}_sde_{sde_type}_itr_{num_itr}_stage_{stage}'
     running_comment = f'python main.py --problem-name {name} --forward-net toy  --backward-net toy --log-tb --gpu {gpu}'
     if sde_type == 've':
-        sigma_min = np.random.choice([0.03, 0.1, 0.3])
-        sigma_max = np.random.choice([0.3, 0.6, 1, 3, 6])
+        sigma_min = np.random.choice([0.03, 0.1, 0.2, 0.3])
+        sigma_max = np.random.choice([0.4, 0.6, 0.8, 1])
         running_comment += f' --sigma-min {sigma_min} --sigma-max {sigma_max}'
         output_folder += f'_sigma_min_{sigma_min}_max_{sigma_max}'
     elif sde_type == 'vp':
-        beta_min = np.random.choice([0.03, 0.05, 0.1])
-        beta_max = np.random.choice([0.6, 1, 3])
+        beta_min = np.random.choice([0.05, 0.1, 0.2, 0.3])
+        beta_max = np.random.choice([0.6, 0.8, 1, 1.3, 1.6])
         running_comment += f' --beta-min {beta_min} --beta-max {beta_max}'
         output_folder += f'_beta_min_{beta_min}_max_{beta_max}'
     running_comment += f' --lr {lr} --lr-gamma {lr_gamma} --t0 {t0} --sde-type {sde_type} --num-itr {num_itr} --num-stage {stage} --dir {output_folder}'
