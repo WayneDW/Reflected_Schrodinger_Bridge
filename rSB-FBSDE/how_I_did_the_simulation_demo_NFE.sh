908a5093f7c33d1fcbb2d4c8733922564625aba8
Wei Deng <greyman@live.cn>
2023-11-18 10:11:02 -0500

    how to do simulation demo to demonstrate the improvement in NFEs

---
+++ rSB-FBSDE/how_I_did_the_simulation_demo_NFE.sh
@@ -0,0 +1,9 @@
+python main.py --problem-name spiral --lr 0.0002 --interval 10 --train-bs-x 2000 --gpu 1 --seed 157 --reset-stage 2 --dir flower_spiral_lr_0.0002_reset_stage_2_bs_2000_seed_157_time_10_snap4 --log-tb
+
+python main.py --problem-name spiral --lr 0.0002 --interval 20 --train-bs-x 2000 --gpu 1 --seed 157 --reset-stage 2 --dir flower_spiral_lr_0.0002_reset_stage_2_bs_2000_seed_157_time_20_snap4 --log-tb
+
+python main.py --problem-name spiral --lr 0.0002 --interval 40 --train-bs-x 2000 --gpu 1 --seed 157 --reset-stage 2 --dir flower_spiral_lr_0.0002_reset_stage_2_bs_2000_seed_157_time_40_snap4 --log-tb
+
+python main.py --problem-name spiral --lr 0.0002 --interval 80 --train-bs-x 2000 --gpu 1 --seed 157 --reset-stage 2 --dir flower_spiral_lr_0.0002_reset_stage_2_bs_2000_seed_157_time_80_snap4 --log-tb
+
+python main.py --problem-name spiral --lr 0.0002 --interval 160 --train-bs-x 2000 --gpu 1 --seed 157 --reset-stage 2 --dir flower_spiral_lr_0.0002_reset_stage_2_bs_2000_seed_157_time_160_snap4 --log-tb
