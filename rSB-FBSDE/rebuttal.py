f45b552c2d6ea0fbd4afa37a9a526e302e849998
Wei Deng <greyman@live.cn>
2023-11-17 19:26:39 -0500

    .

--- rSB-FBSDE/rebuttal.py
+++ rSB-FBSDE/rebuttal.py
@@ -10,13 +10,15 @@ if len(sys.argv) == 2:
 
 print(f'set up GPU {gpu}')
 
-for _ in range(20):
+
+for _ in range(5):
     seed = np.random.choice(np.arange(1000))   
-    lr = np.random.choice([4e-4, 3e-4])
+    lr = np.random.choice([5e-4, 4e-4, 4e-4, 3e-4, 3e-4, 2e-4])
+    reset_stage = np.random.choice([2, 3, 4])
     for interval in [10, 20, 40, 80]:
-        FolderName = f'flower_spiral_lr_{lr}_bs_2000_seed_{seed}_time_{interval}'
+        FolderName = f'flower_spiral_lr_{lr}_reset_stage_{reset_stage}_bs_2000_seed_{seed}_time_{interval}'
         print(FolderName)
         try:
-            os.system(f'python main.py --problem-name spiral --lr {lr} --interval {interval} --train-bs-x 2000 --gpu {gpu} --seed {seed} --dir {FolderName} --log-tb')
+            os.system(f'python main.py --problem-name spiral --lr {lr} --interval {interval} --train-bs-x 2000 --gpu {gpu} --seed {seed} --reset-stage {reset_stage} --dir {FolderName} --log-tb')
         except:
             continue
